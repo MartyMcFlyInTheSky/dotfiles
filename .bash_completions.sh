d3ee321fad9bf3f96942ede1e3425eefbd5f77bf
sbeer@meteomatics.com <sbeer@meteomatics.com>
2024-08-11 14:53:17 +0200

    feat(bash-completions): Added completions for executables in bash and reversed history fzf on tab

--- .bash_completions.sh
+++ .bash_completions.sh
@@ -1,3 +1,15 @@
+# Bring up history selection
+__cmp_history() {
+    local cur="$2"
+    local prev="$3"
+
+    local selected_command=$(history | fzf --tac --no-sort | sed 's/^[ ]*[0-9]*[ ]*//')
+    if [[ -n "$selected_command" ]]; then
+      COMPREPLY=( "$selected_command" )
+    else
+      COMPREPLY=()
+    fi
+}
 
 # Complete --target of cmake command
 __cmp_cmake() {
@@ -28,12 +40,33 @@ __cmp_cmake() {
 
     local target=$(cmake --build "$build_dir" --target help 2>/dev/null | awk '/^[a-zA-Z0-9_-]+:/ {print $1}' | sed 's/://' | fzf --query "$cur")
 
-  if [[ -n "$selected_dir" ]]; then
+  if [[ -n "$target" ]]; then
     COMPREPLY=( "$target" )
   else
     COMPREPLY=()
   fi
 }
 
+
+__cmp_exec() {
+    local curr="$2"
+    case "$curr" in
+        ./*)
+            local selected=$(find "." ! -type d -executable | fzf --query "$curr")
+        if [[ -n "$selected" ]]; then
+            COMPREPLY=( "$selected" )
+        else
+            COMPREPLY=()
+        fi
+    esac
+}
+
+
+complete -I -F __cmp_exec -o bashdefault
+
 complete -o nospace -F __cmp_cmake cmake
+
+complete -F __cmp_history -E
+complete -F __cmp_exec bash
+
 
