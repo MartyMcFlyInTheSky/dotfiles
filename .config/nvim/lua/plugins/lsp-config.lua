a59754d927bbe9efdbb07bf79c605f250de325ec
sbeer@meteomatics.com <sbeer@meteomatics.com>
2024-12-28 16:39:33 +0100

    feat(nvim-completions): Added auto-complete and code snippets to nvim

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,6 +1,5 @@
 return {
 	{
-
 		"williamboman/mason.nvim",
 		opts = {},
 	},
@@ -21,10 +20,15 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = { "williamboman/mason-lspconfig.nvim" },
 		config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig["lua_ls"].setup({})
+			lspconfig["lua_ls"].setup({
+                capabilities = capabilities,
+            })
 			lspconfig["bashls"].setup({
 				filetypes = { "sh", "bash" },
+                capabilities = capabilities,
 			})
             -- lspconfig["rust_analyzer"].setup({
             --     root_dir = lspconfig.util.root_pattern("Cargo.toml"),
