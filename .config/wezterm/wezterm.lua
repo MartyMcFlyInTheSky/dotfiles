2112622f591931edd71121d2943a102a73fc2a55
sbeer@meteomatics.com <sbeer@meteomatics.com>
2025-01-04 12:03:40 +0100

    refactor(wezterm-config): Clean up config file

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,6 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = {}
--- Disable wezterm keybindings as we use tmux as our main multiplexer
--- config.disable_default_key_bindings = true
 
 -- I need to come back to this, but for now deactivate the automatic reassignement
 -- of the socket (https://wezfurlong.org/wezterm/config/lua/config/mux_enable_ssh_agent.html)
@@ -13,54 +11,14 @@ config.leader = {
     key = ' ',
     mods = 'CTRL',
 }
+
 config.keys = {
-    -- Close (stuck) ssh session (https://apple.stackexchange.com/questions/35524/what-can-i-do-when-my-ssh-session-is-stuck) 
+    -- Close (stuck) ssh session (https://apple.stackexchange.com/questions/35524/what-can-i-do-when-my-ssh-session-is-stuck)
     {
         key = 'k',
         mods = 'LEADER',
         action = act.SendString('\r~.'),
     },
---    {
---        key = 'C',
---        mods = 'CTRL|SHIFT',
---        action = wezterm.action_callback(function(window, pane)
---            local process_name = pane:get_foreground_process_name()
---            wezterm.log_info('process name = ' .. process_name)
---            if process_name == 'nvim' then
---                wezterm.log_info('c + shift + ctrl pressed')
---                wezterm.action.SendString('\033[99;6u')
---            else
---                wezterm.action.CopyTo('Clipboard')
---            end
---        end),
---    },
-    -- {
-    --     key = "-",
-    --     mods = "CTRL",
-    --     action = wezterm.action_callback(function(win, pane)
-    --         win:perform_action(act.SendString("Hello"), pane)
-    --         wezterm.log_info 'Hello from callback!'
-    --         wezterm.log_info(
-    --             'WindowID:',
-    --             win:window_id(),
-    --             'PaneID:',
-    --             pane:pane_id()
-    --         )
-    --     end),
-    -- },
-    -- Ctrl+Shift+C: Send a special escape sequence only if Neovim is in the foreground
-    -- {
-    --     key = 'C',
-    --     mods = 'CTRL|SHIFT',
-    --     action = wezterm.action_callback(function(win, pane)
-    --         if pane:is_alt_screen_active() then
-    --             -- Send the custom escape sequence
-    --             win:perform_action(act.SendString("\x1b[99;5u"), pane)
-    --         else
-    --             -- Otherwise, do nothing or other action if needed
-    --         end
-    --     end),
-    -- },
 }
 
 config.font = wezterm.font_with_fallback {
@@ -72,9 +30,9 @@ config.font = wezterm.font_with_fallback {
 
 -- config.color_scheme = 'Dracula'
 config.colors = {
-    foreground = "#B3BBE5",  -- periwinkle
-    background = "#1A170E",  -- smoky_black
-    cursor_bg = "#d4d4d4",   -- editorCursor.foreground in VS Code
+    foreground = "#B3BBE5", -- periwinkle
+    background = "#1A170E", -- smoky_black
+    cursor_bg = "#d4d4d4",  -- editorCursor.foreground in VS Code
     cursor_border = "#d4d4d4",
     cursor_fg = "#1e1e1e",
     selection_bg = "#264f78", -- editor.selectionBackground in VS Code
@@ -102,48 +60,4 @@ config.colors = {
     },
 }
 
--- config.background = {
---     {
---         source={
---             File={
---                 path = wezterm.config_dir .. "/nodes.gif",
---                 speed=0.2
---             }
---         },
---         repeat_x = 'Mirror',
---         hsb = dimmer,
---         -- When the viewport scrolls, move this layer 10% of the number of
---         -- pixels moved by the main viewport. This makes it appear to be
---         -- further behind the text.
---         attachment = { Parallax = 0.1 },
---     }
--- }
-
-
--- Random backgroundcolors
-math.randomseed(os.time())
-
-local hue = math.random(0, 360);
-
--- config.window_background_gradient = {
---         colors = {
---             string.format("hwt123¨(%f,%f%%,0%%)",
---             hue,
---             math.random(25, 40)),
---             string.format("hwb(%f,0%%,%f%%)",
---             hue,
---             math.random(70, 100)),
---         },
---         blend = "Oklab",
---         orientation = {
---             Radial = {
---                 cx = 0.8,
---                 cy = 0.8,
---                 radius = 1.2,
---             }
---         },
---     }
-
-
 return config
-
