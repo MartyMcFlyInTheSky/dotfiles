2a4d2e9d6422272cda8ce9af79a0a9c32313c1ff
sbeer@meteomatics.com <sbeer@meteomatics.com>
2025-01-17 06:15:36 +0100

    feat(nvim-treesitter): Enable autoinstalling parsers for treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -26,6 +26,7 @@ return {
                     "python",
                 },
                 sync_install = false,
+                auto_install = true,
                 highlight = {
                     enable = true,
                     additional_vim_regex_highlighting = false,
