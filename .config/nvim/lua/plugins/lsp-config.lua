b4338893ee445b46ffbde268194b1de74670059d
sbeer@meteomatics.com <sbeer@meteomatics.com>
2025-02-22 13:54:06 +0100

    fix(nvim-autoformat): Disable lsp autoformat on save

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -87,20 +87,20 @@ local on_attach = function(client, bufnr)
     -- end
 
     -- Autoformat command
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({
-            group = augroup,
-            buffer = bufnr,
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-        })
-    end
+    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    -- if client.supports_method("textDocument/formatting") then
+    --     vim.api.nvim_clear_autocmds({
+    --         group = augroup,
+    --         buffer = bufnr,
+    --     })
+    --     vim.api.nvim_create_autocmd("BufWritePre", {
+    --         group = augroup,
+    --         buffer = bufnr,
+    --         callback = function()
+    --             vim.lsp.buf.format({ bufnr = bufnr })
+    --         end,
+    --     })
+    -- end
 end
 
 
