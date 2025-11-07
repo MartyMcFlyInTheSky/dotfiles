133b38c6c78a466262c3e91a184d8e4385568257
sbeer@meteomatics.com <sbeer@meteomatics.com>
2025-11-07 07:09:57 +0100

    feat(nvim-plugin): Add facility to open file on remote

---
+++ .config/nvim/lua/open-on-remote.lua
@@ -0,0 +1,151 @@
+-- Opens a specific line on remote
+-- Adapted from https://github.com/juacker/git-link.nvim/blob/main/lua/git-link/main.lua
+
+local function copy_to_clipboard(text)
+	vim.fn.setreg("+", text)
+	vim.notify("Git URL copied to clipboard", vim.log.levels.INFO)
+end
+
+local function open_url_in_browser(url)
+	local os_name = jit and jit.os or ""
+
+	local command
+	if os_name == "Windows" then
+		command = string.format('start "" "%s"', url) -- Windows
+	elseif os_name == "OSX" then
+		command = string.format("open %s", url) -- macOS
+	elseif os_name == "Linux" then
+		command = string.format("xdg-open %s", url) -- Linux
+	else
+		vim.notify("Unsupported OS: Unable to open URL in browser", vim.log.levels.ERROR)
+		return
+	end
+
+	vim.notify("Opening git URL in browser", vim.log.levels.INFO)
+	vim.fn.jobstart(command, { detach = true })
+end
+
+
+--=============================================================================
+--  Git
+--=============================================================================
+
+local function get_current_branch()
+	local os_name = jit and jit.os or ""
+	local command
+
+	if os_name == "Windows" then
+		command = "git rev-parse --abbrev-ref @{u} 2>NUL"
+	else
+		command = "git rev-parse --abbrev-ref @{u} 2>/dev/null"
+	end
+
+	local output = vim.fn.system(command)
+	if vim.v.shell_error ~= 0 then
+		vim.notify("Could not determine current branch", vim.log.levels.WARN)
+		return "master"
+	end
+
+	local branch = vim.fn.trim(output)
+	local _, branch_name = branch:match("^([^/]+)/(.+)$")
+	return branch_name or "master"
+end
+
+---@param gitcmd string Git command to use
+local function get_remote_url(gitcmd)
+	local remote_url = vim.fn.trim(vim.fn.system(("%s config --get remote.origin.url"):format(gitcmd)))
+	if vim.v.shell_error ~= 0 then
+		vim.notify("Not a git repository or no remote 'origin' found", vim.log.levels.ERROR)
+		return nil
+	end
+
+	local rules = config.get_rules()
+
+	-- Apply URL rewrite rules
+	for _, rule in ipairs(rules) do
+		if remote_url:match(rule.pattern) then
+			local final_url = remote_url:gsub(rule.pattern, rule.replace):gsub("%.git$", "")
+			return final_url, rule.format_url
+		end
+	end
+
+	-- If no rule matches, return nil
+	vim.notify("No matching URL rule found for remote: " .. remote_url, vim.log.levels.ERROR)
+	return nil
+end
+
+local function get_line_range()
+	local mode = vim.fn.mode()
+	if mode:match("[vV]") then
+		local vstart = vim.fn.getpos("v")
+		local vcurrent = vim.fn.getcurpos()
+		return math.min(vstart[2], vcurrent[2]), math.max(vstart[2], vcurrent[2])
+	end
+
+	local current = vim.fn.getcurpos()
+	return current[2], current[2]
+end
+
+local function get_url()
+    local gitcmd = "git"
+
+    -- Check if this is a valid repo
+	vim.fn.trim(vim.fn.system(("%s rev-parse --is-inside-worktree >/dev/null 2>&1")):format(gitcmd))
+	if vim.v.shell_error ~= 0 then
+        -- Try fallback
+        gitcmd = "git --git-dir=$HOME/.myconfig --git-work-tree=$HOME"
+	    vim.fn.trim(vim.fn.system(("%s rev-parse --is-inside-worktree >/dev/null 2>&1"):format(gitcmd)))
+        if vim.v.shell_error ~= 0 then
+		    vim.notify("Not a git repository", vim.log.levels.ERROR)
+		    return nil
+        end
+	end
+
+	local cwd = vim.fn.getcwd()
+	local filename = vim.fn.expand("%:p"):gsub("\\", "/"):gsub("^" .. cwd:gsub("\\", "/") .. "/", "")
+
+	local relative_filename = vim.fn.trim(vim.fn.system(("%s ls-files --full-name %s" ):format(gitcmd, filename)))
+	if vim.v.shell_error ~= 0 or relative_filename == "" then
+		vim.notify("File is not tracked by git", vim.log.levels.ERROR)
+		return nil
+	end
+
+	local remote_url, format_url = get_remote_url(gitcmd)
+	if not remote_url or not format_url then
+		return nil
+	end
+
+	local branch = get_current_branch()
+	local start_line, end_line = get_line_range()
+
+	local params = {
+		branch = branch,
+		file_path = relative_filename,
+		start_line = start_line,
+		end_line = end_line,
+	}
+	return format_url(remote_url, params)
+end
+
+
+--=============================================================================
+--  Commands
+--=============================================================================
+
+local function copy_line_url()
+	local url = get_url()
+	if url then
+		copy_to_clipboard(url)
+	end
+end
+
+local function open_line_url()
+	local url = get_url()
+	if url then
+		open_url_in_browser(url)
+	end
+end
+
+vim.api.nvim_create_user_command('OpenFileOnRemote', open_line_url, {})
+vim.api.nvim_create_user_command('YankFileRemoteLink', copy_line_url, {})
+
